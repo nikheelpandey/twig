0b40587fef044aa213f5104875fd347def7acf9a
Nikhil Pandey <nikhilpandey361@gmail.com>
2017-09-11 23:39:04 +0530

    Update linear_regression_demo.py

--- linear_regression_demo.py
+++ linear_regression_demo.py
@@ -2,6 +2,8 @@ from regression import best_fit
 from regression import predict 
 import numpy as np
 
+# an example showing how the functions are meant to be used
+# like everything, numpy arrays will come handy when it comes to data crunching 
 x = np.array([1,5,7,9,11,12,15],dtype=np.float64)
 y = np.array([2,4,6,9,12,14,18],dtype = np.float64)
 
